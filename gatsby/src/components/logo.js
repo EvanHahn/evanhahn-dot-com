3a185d7e881a8132d753939eac53ab3babc6756b
Evan Hahn <me@evanhahn.com>
2020-04-27 14:54:47 -0500

    Update homepage

---
+++ gatsby/src/components/logo.js
@@ -0,0 +1,24 @@
+import React from "react"
+import { useStaticQuery, graphql } from "gatsby"
+
+export default function Logo() {
+  const data = useStaticQuery(graphql`
+    {
+      file(relativePath: { eq: "logo_white.png" }) {
+        childImageSharp {
+          fixed(width: 512, height: 512, fit: COVER) {
+            src
+          }
+        }
+      }
+    }
+  `)
+
+  return (
+    <img
+      className="Logo"
+      alt=""
+      src={data.file.childImageSharp.fixed.src}
+    />
+  )
+}
--- gatsby/src/pages/index.js
+++ gatsby/src/pages/index.js
@@ -1,21 +1,91 @@
 import React from "react"
-import { Link } from "gatsby"
-
-import Layout from "../components/layout"
-import Image from "../components/image"
+import AlternateEmailIcon from "@material-ui/icons/AlternateEmail"
+import GithubIcon from "@material-ui/icons/GitHub"
+import TwitterIcon from "@material-ui/icons/Twitter"
+import KeybaseIcon from "@material-ui/icons/VpnKey"
+import AccountCircleIcon from "@material-ui/icons/AccountCircle"
+import MenuBookIcon from "@material-ui/icons/MenuBook"
+import ListIcon from "@material-ui/icons/List"
+import CodeIcon from "@material-ui/icons/Code"
 import SEO from "../components/seo"
+import Logo from "../components/logo"
+import MastodonIcon from "../components/MastodonIcon"
+import "../components/index.css"
+
+const IconLinks = ({ children }) => <ul className="IconLinks">{children}</ul>
+
+const IconLink = ({ href, target, icon, children }) => {
+  return (
+    <li className="IconLink">
+      <a href={href} target={target} rel="noreferrer noopener">
+        {icon}
+        <span>{children}</span>
+      </a>
+    </li>
+  )
+}
 
 const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link>
-  </Layout>
+  <div className="IndexPage">
+    <SEO title="Evan Hahn" />
+
+    <header>
+      <Logo />
+      <h1>
+        I'm Evan Hahn,
+        <br />a programmer.
+      </h1>
+    </header>
+
+    <IconLinks>
+      <IconLink href="/about" icon={<AccountCircleIcon />}>
+        About me
+      </IconLink>
+      <IconLink href="/guides" icon={<MenuBookIcon />}>
+        Guides and books
+      </IconLink>
+      <IconLink href="/projects" icon={<CodeIcon />}>
+        Projects
+      </IconLink>
+      <IconLink href="/blog" icon={<ListIcon />}>
+        Blog
+      </IconLink>
+    </IconLinks>
+
+    <IconLinks>
+      <IconLink href="mailto:me@evanhahn.com" icon={<AlternateEmailIcon />}>
+        Email
+      </IconLink>
+      <IconLink
+        href="https://bigshoulders.city/@EvanHahn"
+        target="_blank"
+        icon={<MastodonIcon />}
+      >
+        Mastodon
+      </IconLink>
+      <IconLink
+        href="https://twitter.com/EvanHahn"
+        target="_blank"
+        icon={<TwitterIcon />}
+      >
+        Twitter
+      </IconLink>
+      <IconLink
+        href="https://github.com/EvanHahn"
+        target="_blank"
+        icon={<GithubIcon />}
+      >
+        GitHub
+      </IconLink>
+      <IconLink
+        href="https://keybase.io/EvanHahn"
+        target="_blank"
+        icon={<KeybaseIcon />}
+      >
+        Keybase
+      </IconLink>
+    </IconLinks>
+  </div>
 )
 
 export default IndexPage
