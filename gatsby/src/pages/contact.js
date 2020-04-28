b784782d9699fbda337eb19eaaffd8f5b4998b13
Evan Hahn <me@evanhahn.com>
2020-04-28 11:39:42 -0500

    Update contact page

--- gatsby/src/pages/contact.js
+++ gatsby/src/pages/contact.js
@@ -1,10 +1,5 @@
 import React from "react"
-import { IconLink } from "../components/iconlinks"
-import AlternateEmailIcon from "@material-ui/icons/AlternateEmail"
-import TwitterIcon from "@material-ui/icons/Twitter"
-import KeybaseIcon from "@material-ui/icons/VpnKey"
 import SEO from "../components/seo"
-import MastodonIcon from "../components/MastodonIcon"
 import ContentPageHeader from "../components/header"
 import "../components/index.css"
 
@@ -16,36 +11,63 @@ export default function ContactPage() {
       <div className="ContentPageHeaderAndMainWrapper">
         <main className="ContentPageMain">
           <h1>Contact</h1>
-          <p>Contact me! If you want!</p>
-          <ul style={{ padding: 0 }}>
-            <IconLink
-              href="mailto:me@evanhahn.com"
-              icon={<AlternateEmailIcon />}
-            >
-              Email
-            </IconLink>
-            <IconLink
-              href="https://bigshoulders.city/@EvanHahn"
-              target="_blank"
-              icon={<MastodonIcon />}
-            >
-              Mastodon
-            </IconLink>
-            <IconLink
-              href="https://twitter.com/EvanHahn"
-              target="_blank"
-              icon={<TwitterIcon />}
-            >
-              Twitter
-            </IconLink>
-            <IconLink
+
+          <p>
+            You can <strong>email me</strong> at{" "}
+            <a href="mailto:me@evanhahn.com">me@evanhahn.com</a>.
+          </p>
+
+          <p>
+            You can message me on <strong>Keybase</strong> at{" "}
+            <a
               href="https://keybase.io/EvanHahn"
               target="_blank"
-              icon={<KeybaseIcon />}
+              rel="noreferrer noopener"
             >
-              Keybase
-            </IconLink>
-          </ul>
+              keybase.io/EvanHahn
+            </a>
+            .
+          </p>
+
+          <p>
+            You can follow me on the <strong>Fediverse</strong> at{" "}
+            <a
+              href="https://bigshoulders.city/@EvanHahn"
+              target="_blank"
+              rel="noreferrer noopener"
+            >
+              @EvanHahn@bigshoulders.city
+            </a>{" "}
+            and on <strong>Twitter</strong> at{" "}
+            <a
+              href="https://twitter.com/EvanHahn"
+              target="_blank"
+              rel="noreferrer noopener"
+            >
+              @EvanHahn
+            </a>
+            .
+          </p>
+
+          <p>
+            I'm on <strong>GitHub</strong> at{" "}
+            <a
+              href="https://github.com/EvanHahn"
+              target="_blank"
+              rel="noreferrer noopener"
+            >
+              github.com/EvanHahn
+            </a>{" "}
+            and I'm also on <strong>StackOverflow</strong>{" "}
+            <a
+              href="https://stackoverflow.com/users/804100/evan-hahn"
+              target="_blank"
+              rel="noreferrer noopener"
+            >
+              here
+            </a>
+            .
+          </p>
         </main>
 
         <ContentPageHeader />
