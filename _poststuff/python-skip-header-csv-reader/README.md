3411557a14697bca4fe6184717dadebf61cce1ea
Evan Hahn <me@evanhahn.com>
2020-04-28 17:14:46 -0500

    Remove easy-to-delete files from Jekyll land

--- _poststuff/python-skip-header-csv-reader/README.md
+++
@@ -1,8 +0,0 @@
-To run this sucker:
-
-```
-python2 reader.py
-python3 reader.py
-```
-
-Neither should return errors.
--- _poststuff/python-skip-header-csv-reader/reader.py
+++
@@ -1,7 +0,0 @@
-import csv
-
-with open('data.csv', 'r') as csvfile:
-    csvreader = csv.reader(csvfile)
-    next(csvreader)
-    for row in csvreader:
-        assert(row[0] != 'Date')
