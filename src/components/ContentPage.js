b2a221cbf7735365ae4c4868913b8af9edd75ecc
Evan Hahn <me@evanhahn.com>
2020-04-29 13:19:18 -0500

    Move ContentPageHeader into the ContentPage component file

--- src/components/ContentPage.js
+++ src/components/ContentPage.js
@@ -1,8 +1,43 @@
 import React from "react";
+import { Link } from "gatsby";
 import SEO from "../components/seo";
-import ContentPageHeader from "../components/header";
+import Logo from "./logo";
+import { IconLinks, IconLink } from "./iconlinks";
+import AlternateEmailIcon from "@material-ui/icons/AlternateEmail";
+import AccountCircleIcon from "@material-ui/icons/AccountCircle";
+import MenuBookIcon from "@material-ui/icons/MenuBook";
+import ListIcon from "@material-ui/icons/List";
+import CodeIcon from "@material-ui/icons/Code";
 import DateElement from "../components/DateElement";
 import "../components/index.css";
+
+function ContentPageHeader() {
+  return (
+    <header className="ContentPageHeader">
+      <Link to="/">
+        <Logo />
+      </Link>
+
+      <IconLinks>
+        <IconLink useGatsbyLink href="/about" icon={<AccountCircleIcon />}>
+          About
+        </IconLink>
+        <IconLink useGatsbyLink href="/contact" icon={<AlternateEmailIcon />}>
+          Contact
+        </IconLink>
+        <IconLink useGatsbyLink href="/projects" icon={<CodeIcon />}>
+          Projects
+        </IconLink>
+        <IconLink useGatsbyLink href="/guides" icon={<MenuBookIcon />}>
+          Guides
+        </IconLink>
+        <IconLink useGatsbyLink href="/blog" icon={<ListIcon />}>
+          Blog
+        </IconLink>
+      </IconLinks>
+    </header>
+  );
+}
 
 function ContentPageDate({ date }) {
   return (
